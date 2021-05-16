5e87073e130c16c88b0bf565d6bf357e937cda68
Carl de Guia <carl.2795@gmail.com>
2021-05-16 15:21:48 +0800

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import React, { useRef } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Hydrate } from 'react-query/hydration';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
